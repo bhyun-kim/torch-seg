08910a0a2718ccb4cf7824cefbb64f5cabf4e879
uos-bhkim <shdik2002@uos.ac.kr>
2023-02-09 09:01:17 +0900

    Add ImageNet load fail exception

--- datasets/imagenet.py
+++ datasets/imagenet.py
@@ -19,13 +19,16 @@ class ImageNetDataset(datasets.ImageNet):
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         """
-        path, target = self.samples[index]
-        img = self.loader(path)
+        try : 
+            path, target = self.samples[index]
+            img = self.loader(path)
 
-        sample = {'image': img, 'target': target} 
+            sample = {'image': img, 'target': target} 
 
-        if self.transform is not None:
-            sample = self.transform(sample)
+            if self.transform is not None:
+                sample = self.transform(sample)
+        except: 
+            return None
 
         return sample
 
